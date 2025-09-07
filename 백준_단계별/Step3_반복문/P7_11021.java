888b61c06f0b99c416605d74983f197525d18ebe
이지민 <1zzimy@naver.com>
2025-09-07 13:56:10 +0900

    11021번 A+B - 7

---
+++ 백준_단계별/Step3_반복문/P7_11021.java
@@ -0,0 +1,30 @@
+package 백준_단계별.Step3_반복문;
+
+import java.io.*;
+import java.util.*;
+
+public class P7_11021 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		int T = Integer.parseInt(br.readLine());
+
+		StringBuilder sb = new StringBuilder();
+
+		for(int i = 1; i <= T; i++) {
+			st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+
+			sb.append("Case #").append(i).append(": ").append(A + B).append("\n");
+		}
+
+		br.close();
+
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
+}
